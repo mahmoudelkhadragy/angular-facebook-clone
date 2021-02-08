9208f7e042b7b3a6eaf3504e02d2ac26ef3e28b2
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-02-08 15:52:00 +0200

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+};
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDY9VQlA3inVHX2C5oQMPnWuZJwMHQ9WFo',
+  authDomain: 'ng-facebook-clone.firebaseapp.com',
+  projectId: 'ng-facebook-clone',
+  storageBucket: 'ng-facebook-clone.appspot.com',
+  messagingSenderId: '943393703160',
+  appId: '1:943393703160:web:2ce470be5b6ec62376270d',
+  measurementId: 'G-PH6095LRDY',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
 
 /*
@@ -14,3 +14,13 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDY9VQlA3inVHX2C5oQMPnWuZJwMHQ9WFo',
+  authDomain: 'ng-facebook-clone.firebaseapp.com',
+  projectId: 'ng-facebook-clone',
+  storageBucket: 'ng-facebook-clone.appspot.com',
+  messagingSenderId: '943393703160',
+  appId: '1:943393703160:web:2ce470be5b6ec62376270d',
+  measurementId: 'G-PH6095LRDY',
+};
